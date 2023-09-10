a1976366d42a08ed71366f86f1cb55dd8406e474
Suyash Srivastava <suyash.srt@outlook.com>
2023-09-10 21:44:14 +0530

    serializer added

---
+++ chatapp/serializers.py
@@ -0,0 +1,37 @@
+from django.contrib.auth.models import User
+from rest_framework import serializers
+
+class UserRegistrationSerializer(serializers.ModelSerializer):
+    password = serializers.CharField(write_only=True, required=True, style={'input_type': 'password'})
+
+    class Meta:
+        model = User
+        fields = ['username', 'password', 'email', 'first_name', 'last_name']
+
+    def create(self, validated_data):
+        # Create a new User object with the validated data
+        user = User.objects.create_user(**validated_data)
+        return user
+
+class UserLoginSerializer(serializers.Serializer):
+    username = serializers.CharField()
+    password = serializers.CharField(style={'input_type': 'password'})
+
+    def validate(self, data):
+        username = data.get('username')
+        password = data.get('password')
+
+        if username and password:
+            user = User.objects.filter(username=username).first()
+
+            if user is None:
+                raise serializers.ValidationError("User not found.")
+            
+            if not user.check_password(password):
+                raise serializers.ValidationError("Incorrect password.")
+
+            data['user'] = user
+        else:
+            raise serializers.ValidationError("Must include 'username' and 'password' fields.")
+
+        return data
